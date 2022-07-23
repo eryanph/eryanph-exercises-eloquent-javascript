927122f8973ea5d28814b962a45e4e0755b0fc2a
eryan_ph <eryan.gamlanga@gmail.com>
2022-07-23 20:23:32 +0800

    Add 05_higher_order/flattening answer

---
+++ 05_higher_order/flattening.js
@@ -0,0 +1,10 @@
+/**
+ * https://eloquentjavascript.net/05_higher_order.html#i_aIOczlLyX1
+ * 
+ * Flattening
+ * Use the reduce method in combination with the concat method to “flatten” an array of arrays into a
+ * single array that has all the elements of the original arrays.
+ */
+
+const arrays = [[1, 2, 3], [4, 5], [6]];
+console.log(arrays.reduce((a, b) => a.concat(b)));
