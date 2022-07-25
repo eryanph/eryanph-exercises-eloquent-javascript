d8ce2896f0c26242ede04e7641bcfff555248da8
eryan_ph <eryan.gamlanga@gmail.com>
2022-07-25 17:27:42 +0800

    Add 08_error/retry answer

---
+++ 08_error/retry.js
@@ -0,0 +1,34 @@
+/**
+ * https://eloquentjavascript.net/08_error.html#i_n1zYouiAfX
+ * 
+ * Retry
+ * Say you have a function primitiveMultiply that in 20 percent of cases multiplies two numbers and in the other
+ * 80 percent of cases raises an exception of type MultiplicatorUnitFailure. Write a function that wraps this
+ * clunky function and just keeps trying until a call succeeds, after which it returns the result.
+ * 
+ * Make sure you handle only the exceptions you are trying to handle.
+ */
+
+class MultiplicatorUnitFailure extends Error {};
+
+const primitiveMultiply = (a, b) => {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure('Klunk');
+  }
+};
+
+const reliableMultiply = (a, b) => {
+  while (true) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (e) {
+      if (!(e instanceof MultiplicatorUnitFailure)) {
+        throw e
+      }
+    }
+  }
+};
+
+console.log(reliableMultiply(8, 8));
